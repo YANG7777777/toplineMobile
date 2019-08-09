da9b1b84211c978d181691bfb56b53eec41c6645
yangjia <yangjia1416@163.com>
2019-08-09 21:52:38 +0800

    初始化配置完成

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,21 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
+//      sep 路径中的分割符 为了适配不同的操作系统
+const { sep } = require('path')
+
+module.exports = ({ file }) => {
+  // 编译的时候处理不同的样式文件 可能是 css/less/sass/vue
+
+  // 判断文件路径中是否包含 vant , 是否是vant提供的样式 | 如果是设置 rootValue = 37.5 如果是自己写的设置 rootValue = 75
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
+    }
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view/>
   </div>
 </template>
